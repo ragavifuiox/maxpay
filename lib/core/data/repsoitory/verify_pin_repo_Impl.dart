171ef4b68055382787d47e80ddc3345626a31991
Aswanth <aswanth.novelx@gmail.com>
2026-07-22 11:01:11 +0530

    refactor: integrate AppLogger and improve error handling in VerifyPinRepoImpl

--- lib/core/data/repsoitory/verify_pin_repo_Impl.dart
+++ lib/core/data/repsoitory/verify_pin_repo_Impl.dart
@@ -5,6 +5,7 @@ import 'package:maxpay/core/data/model/erify_pin_model.dart';
 import 'package:maxpay/core/domain/repository/verify_pin_repository.dart';
 import 'package:maxpay/core/error/failure.dart';
 import 'package:maxpay/core/services/api_services.dart';
+import 'package:maxpay/core/utils/logg_helper.dart';
 
 class VerifyPinRepoImpl implements VerifyPinRepository {
   final ApiService apiService;
@@ -12,21 +13,25 @@ class VerifyPinRepoImpl implements VerifyPinRepository {
   VerifyPinRepoImpl(this.apiService);
 
   @override
-  Future<Either<Failure, VerifyPin>> verifypin({
-    required String pin,
-  }) async {
+  Future<Either<Failure, VerifyPin>> verifypin({required String pin}) async {
     try {
       final response = await apiService.post(
         ApiRoutes.verifypin,
-        data: {
-          "pin": pin,
-        },
+        data: {"pin": pin},
       );
 
       final model = VerifyPin.fromJson(response);
       return Right(model);
     } on DioException catch (e) {
-      print(e.response);
+      AppLogger.logError(e.response);
+      return Left(
+        ServerFailure(
+          message:
+              e.response?.data['message'].toString() ??
+              "An Authorised Error Occured",
+        ),
+      );
+    } catch (e) {
       return Left(ServerFailure(message: e.toString()));
     }
   }
